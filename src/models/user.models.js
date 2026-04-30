b9143e7873cbba9fc08a9fc515dc833299bba56f
Aadi Patel <aadipatel0042@gmail.com>
2026-04-30 09:39:49 +0530

    created user Schema

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,33 +1,46 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const user = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, "Email is needed"],
         trim: true,
         lowercase: true,
         match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Please enter a valid email"],
-        unique:[true, "Email already exists"]
+        unique: [true, "Email already exists"]
     },
     name: {
         type: String,
         required: [true, "Name is needed for creating an account"],
         trim: true
     },
-    password:{
+    password: {
         type: String,
-        required: [true, "Password is needed for creating an account"],
+        required: [true, "Password is needed"],
         trim: true,
         minlength: [6, "Password must be at least 6 characters long"],
         select: false
     }
-},{
-    timestamps:true
+}, {
+    timestamps: true
 });
 
-userSchema.pre("save", async function(next){
-    if(!this.isModified("password")){
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
         return next();
     }
 
-module.exports = mongoose.model("User", user);
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
+});
+
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
